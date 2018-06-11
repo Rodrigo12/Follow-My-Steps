10be30ef16dc611bf9160c13024ec71addf41c30
Rodrigo França Martins <rodrigofranca@Rodrigos-MBP.home>
2018-06-11 20:22:33 +0100

    Deleted folders

--- Projecto/client/js/conversions/photo.js
+++
@@ -1,6 +0,0 @@
-function convertToPhotoObj(file, data, description){
-  var timestamp = file.lastModifiedDate;
-  if (description == "")
-    description = "-";
-  return [ file.name, description];
-}
